4fe5b5f046e3ef405db73633b3fa5d17a7639f57
AP-Abhishek <abhishekap12005@gmail.com>
2026-01-02 21:19:22 +0530

    feat: Added default roles.

--- frontend/src/pages/Role.tsx
+++ frontend/src/pages/Role.tsx
@@ -45,7 +45,7 @@ const Role = () => {
           "Content-Type": "application/json",
           "Authorization": `Bearer ${localStorage.getItem("token")}`
         },
-        body: JSON.stringify({ name: newRoleName }),
+        body: JSON.stringify({ name: newRoleName.toLowerCase() }),
       });
       const data = await res.json();
       if (res.ok) {
@@ -135,22 +135,32 @@ const Role = () => {
                   <span className="text-xs font-semibold text-stone-400 italic">ID: {role._id}</span>
                 </div>
                 <div className="flex gap-2">
-                  <button
-                    onClick={() => openEditModal(role)}
-                    className="p-2 text-sky-600 hover:bg-sky-100 rounded-full transition-colors"
-                  >
-                    <svg xmlns="www.w3.org" className="size-5" viewBox="0 0 20 20" fill="currentColor">
-                      <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-                    </svg>
-                  </button>
-                  <button
-                    onClick={() => setDeletingRole(role)}
-                    className="p-2 text-red-600 hover:bg-red-50 rounded-full transition-colors"
-                  >
-                    <svg xmlns="www.w3.org" className="size-5" viewBox="0 0 20 20" fill="currentColor">
-                      <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
-                    </svg>
-                  </button>
+                  {
+                    role.name == "admin" || role.name == "user"
+                      ? (
+                        <span className="text-sm text-stone-500 font-semibold italic">Default Role</span>
+                      )
+                      : (
+                        <>
+                          <button
+                            onClick={() => openEditModal(role)}
+                            className="p-2 text-sky-600 hover:bg-sky-100 rounded-full transition-colors"
+                          >
+                            <svg xmlns="www.w3.org" className="size-5" viewBox="0 0 20 20" fill="currentColor">
+                              <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
+                            </svg>
+                          </button>
+                          <button
+                            onClick={() => setDeletingRole(role)}
+                            className="p-2 text-red-600 hover:bg-red-50 rounded-full transition-colors"
+                          >
+                            <svg xmlns="www.w3.org" className="size-5" viewBox="0 0 20 20" fill="currentColor">
+                              <path fillRule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
+                            </svg>
+                          </button>
+                        </>
+                      )
+                  }
                 </div>
               </div>
             ))}
