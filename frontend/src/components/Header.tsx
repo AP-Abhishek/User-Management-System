3ed54ea34ce75dc12d525fa9c1e3bea4f719a545
AP-Abhishek <abhishekap12005@gmail.com>
2026-01-02 19:31:29 +0530

    style: Chnaged header styles

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header = () => {
   const { user } = useAuth();
   return (
     <>
-      <header className="w-full px-12 py-4 flex items-center justify-between">
+      <header className="w-full px-12 py-4 flex items-center justify-between bg-background border-b-4 border-sky-900">
         <section className="flex-center">
           <Link to={"/"}>
             <img
@@ -15,7 +15,7 @@ const Header = () => {
               alt="logo"
             />
           </Link>
-          <h1 className="text-3xl font-semibold mx-4">User Management System</h1>
+          <h1 className="text-3xl font-semibold mx-4 cursor-default">User Management System</h1>
         </section>
         <section className="flex gap-8">
           <Navbar />
