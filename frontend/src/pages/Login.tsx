64a95f8148cb98b7fb005392f34b6eacbc330157
AP-Abhishek <abhishekap12005@gmail.com>
2026-01-03 19:01:30 +0530

    fix: Fixed backend url in login page.

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -26,7 +26,7 @@ const Login = () => {
 		}
 
 		try {
-			const res = await fetch("http://localhost:5000/api/users/login", {
+			const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/users/login`, {
 				method: "POST",
 				headers: {
 					"Content-Type": "application/json",
