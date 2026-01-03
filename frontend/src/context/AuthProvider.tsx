f82e58c04d8a3fedeb916829137a8db6d11ced9c
AP-Abhishek <abhishekap12005@gmail.com>
2026-01-03 19:37:00 +0530

    fix: Fixed reload bug.

--- frontend/src/context/AuthProvider.tsx
+++ frontend/src/context/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState, type PropsWithChildren } from "react";
+import { createContext, useContext, useState, type PropsWithChildren } from "react";
 import { useNavigate } from "react-router-dom";
 
 interface User {
@@ -21,16 +21,12 @@ type AuthProviderProps = PropsWithChildren;
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
 
-    const [user, setUser] = useState<User | null>(null);
-    const [token, setToken] = useState<string | null>(localStorage.getItem("token"));
-    const navigate = useNavigate();
-
-    useEffect(() => {
+    const [token, setToken] = useState<string | null>(() => localStorage.getItem("token"));
+    const [user, setUser] = useState<User | null>(() => {
         const savedUser = localStorage.getItem("user");
-        if (savedUser && token) {
-            setUser(JSON.parse(savedUser));
-        }
-    }, [token]);
+        return savedUser ? JSON.parse(savedUser) : null;
+    });
+    const navigate = useNavigate();
 
     const login = (newToken: string, userData: User) => {
         setToken(newToken);
