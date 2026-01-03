48dafaf65b995abbe5eca25124cd7757a19db026
AP-Abhishek <abhishekap12005@gmail.com>
2026-01-03 20:30:26 +0530

    style: Responsive Header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,25 +6,25 @@ const Header = () => {
   const { user } = useAuth();
   return (
     <>
-      <header className="w-full px-12 py-4 flex items-center justify-between bg-background border-b-4 border-sky-900">
+      <header className="w-full md:px-12 px-4 md:py-4 py-2 flex items-center justify-between bg-background border-b-4 border-sky-900">
         <section className="flex-center">
           <Link to={"/"}>
             <img
-              className="size-16"
+              className="md:size-16 size-8"
               src="favicon.png"
               alt="logo"
             />
           </Link>
-          <h1 className="text-3xl font-semibold mx-4 cursor-default">User Management System</h1>
+          <h1 className="md:text-3xl text-base font-semibold md:mx-4 mx-1 cursor-default">User Management System</h1>
         </section>
         <section className="flex gap-8">
           <Navbar />
-          <button className="flex-center rounded-full">
-            <p className="mx-2 font-semibold">Hey, {user?.username}</p>
+          <div className="flex-center rounded-full">
+            <p className="md:block hidden mx-2 font-semibold text-base">Hey, {user?.username}</p>
             <Link className="flex-center rounded-full" to="/profile">
-              <img className="flex-center size-14 rounded-full" src="profile.png" alt="profile" />
+              <img className="flex-center md:size-14 size-8 rounded-full" src="profile.png" alt="profile" />
             </Link>
-          </button>
+          </div>
         </section>
       </header>
     </>
