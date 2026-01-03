35e8595ac3e339b1fd2eebfeb42c8d4bf39fa938
AP-Abhishek <abhishekap12005@gmail.com>
2026-01-03 20:52:46 +0530

    style: Responsive Footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -8,12 +8,12 @@ const Footer = () => {
           <h1 className="font-semibold text-lg">User Management System</h1>
         </section>
         <section className="px-8 py-6">
-          <p className="text-center text-stone-700">
+          <p className="text-center text-stone-700 md:text-base text-xs">
             User Management System is a robust full-stack solution built with React and Express. Efficiently manage user lifecycles, monitor active profiles, and enforce security policies through a centralized administrative interface. It was basically built to understand and implement the working of RBAC systems. The applications primary purpose was to build a strong and efficient backend with a simple frontend.
           </p>
         </section>
         <section className="p-2 w-full border-t border-stone-400 flex-center flex-col">
-          <span className="text-stone-900">Contact with the Developer</span>
+          <span className="text-stone-900 md:text-base text-sm">Contact with the Developer</span>
           <section className="p-2 flex-center gap-4">
             <a
               href="https://github.com/AP-Abhishek/User-Management-System"
@@ -21,7 +21,7 @@ const Footer = () => {
               className="transition-all hover:scale-110 ease-linear"
               rel="noreferrer"
             >
-              <img src="github.png" alt="github" className="size-8 rounded-full bg-white" />
+              <img src="github.png" alt="github" className="md:size-8 size-6 rounded-full bg-white" />
             </a>
             <a
               href="https://www.linkedin.com/in/-ap-abhishek-23-03-05-/"
@@ -29,7 +29,7 @@ const Footer = () => {
               className="transition-all hover:scale-110 ease-linear"
               rel="noreferrer"
             >
-              <img src="linkedin.png" alt="linkedin" className="size-8 rounded-full bg-white" />
+              <img src="linkedin.png" alt="linkedin" className="md:size-8 size-6 rounded-full bg-white" />
             </a>
           </section>
         </section>
