406f51e9d56a1d564fc59b49b3c94058c9be72aa
AP-Abhishek <abhishekap12005@gmail.com>
2026-01-04 15:49:46 +0530

    feat: Backend Deployment Files.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,35 @@
 import app from "./app";
 import { connectDB } from "./db/connection";
-import "dotenv/config";
 
-const PORT = process.env.PORT;
+let isConnected = false;
 
-const startServer = async () => {
+const handler = async (req: any, res: any) => {
     try {
-        await connectDB();
-        app.listen(PORT, () => {
-            console.log(`Server listening at ${PORT}`);
-        });
+        if (!isConnected) {
+            await connectDB();
+            isConnected = true;
+        }
+        return app(req, res);
     } catch (err) {
-        console.error(`Failed to start server: ${err}`);
-        process.exit(1);
+        console.error("Handler Error: ", err);
+        res.status(500).send("Internal Server Error");
     }
 };
 
-startServer();
+if (process.env.NODE_ENV !== "production") {
+    import("dotenv/config");
+    const PORT = process.env.PORT;
+    (async () => {
+        try {
+            await connectDB();
+            app.listen(PORT, () => {
+                console.log(`Server listening at ${PORT}`);
+            });
+        } catch (err) {
+            console.error(`Failed to start server: ${err}`);
+            process.exit(1);
+        }
+    })();
+}
+
+export default handler;
